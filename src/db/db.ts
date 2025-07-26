2db51452b5eedbd9051b36f75f18ecd50ebf33ab
MaheshaDinu98 <maheeshadinushan@gmail.com>
2025-07-26 10:46:16 +0530

    implemented user.model.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,3 +1,10 @@
 import {Book} from "../models/book.model";
+import {User} from "../models/user.model";
 
 export const booksList: Book[] = [];
+
+
+export const userList: User[] = [];
+let nextUserId = 1;
+
+export const getNextUserId = () => nextUserId++;
